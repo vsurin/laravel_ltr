2ed48de5ddb174e1485c6017030583b48ceff448
VSurin <V.Surin@evnedev.com>
2018-10-04 11:34:43 +0300

    checnge public puth

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public_html/js')
+   .sass('resources/sass/app.scss', 'public_html/css')
+    .setPublicPath('public_html');

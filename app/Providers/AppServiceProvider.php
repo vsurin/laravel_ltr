966fe210aa6651cc491bf13cb5c339a3dd74fdd3
VSurin <V.Surin@evnedev.com>
2018-10-05 16:09:17 +0300

    add config adminLte and role for menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::before(function ($user) {
+            if ($user->role == 'admin') {
+                return true;
+            }
+        });
     }
 
     /**

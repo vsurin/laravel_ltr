608589715dfdd96bb383155afc5f6277536e7445
VSurin <V.Surin@evnedev.com>
2018-10-17 13:33:10 +0300

    изменил логику seeder

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Project;
 
 class ProjectsTableSeeder extends Seeder
 {
@@ -11,16 +12,27 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i=0; $i<1000; $i++) {
+        $project = new Project;
+
+        for($i = 0; $i < 1000; $i++) {
+            $randStrTime = mt_rand(1152005681,1262055681);
+            $randDay = mt_rand(1, 10);
+            $date = strtotime('+'.$randDay.' days', $randStrTime);
+
+            $textRandom = str_random(mt_rand(1, 15));
+            for($x = 0; $x < 70; $x++) {
+                $textRandom .= ' '.str_random(mt_rand(1, 15));
+            }
+
             DB::table('projects')->insert([
                 'title' => str_random(10),
-                'descrription' => str_random(100),
-                'organization' => str_random(10),
-                'start' => '2018-10-09 14:13:30',
-                'end' => '2018-10-09 14:13:30',
+                'descrription' => $textRandom,
+                'organization' => str_random(mt_rand(4, 10)),
+                'start' => date('Y-m-d', $randStrTime),
+                'end' => date("Y-m-d", $date),
                 'role' => str_random(10),
-                'link' => str_random(10),
-                'type' => str_random(10),
+                'link' => 'http://'.str_random(10).'.com',
+                'type' =>  array_rand($project->getTypes(), 1),
             ]);
         }
     }

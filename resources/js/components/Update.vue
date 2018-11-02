0d985b18e516708596377ad9895e6b1ee582418f
VSurin <V.Surin@evnedev.com>
2018-11-02 17:57:23 +0200

    Валидация на странице обновления - vue

--- resources/js/components/Update.vue
+++ resources/js/components/Update.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
+        <div class="row" v-if="messageIsActive">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div v-bind:class="{'alert-error': hasError, 'alert-success': !hasError}" class="alert alert-dismissible">
+                    <button type="button" class="close" @click="closeMessage">×</button>
+                    <ul>
+                        <li v-for="error in errors">
+                            {{ error }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-xs-6 col-sm-6 col-md-6">
                 <div class="form-group">
@@ -50,14 +62,6 @@
         <div class="col-xs-12 col-sm-12 col-md-12 text-right">
             <button class="btn btn-primary" @click="update">Update</button>
         </div>
-        <div class="row" v-if="messageIsActive">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="alert alert-success alert-dismissible">
-                    <button type="button" class="close" @click="closeMessage">×</button>
-                    {{ result.data.message }}
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
@@ -84,6 +88,11 @@
                     type: 'Work',
                 },
                 messageIsActive: false,
+                hasError: false,
+                errors: [],
+                fieldValidation: [
+                    'title', 'link', 'descrription', 'start', 'end', 'role'
+                ]
             }
         },
         mounted: function () {
@@ -107,13 +116,33 @@
                     type: this.project.type,
                 }).then((response) => {
                     this.result = response,
-                    this.messageIsActive = true
+                    this.validation()
                 }).catch((e) => {
-                        console.error(e)
+                    console.error(e)
                 });
             },
             closeMessage: function() {
                 this.messageIsActive = false;
+            },
+            validation() {
+                this.errors = [];
+
+                for (var i = 0; i < this.fieldValidation.length; i++) {
+                    var item = this.fieldValidation[i];
+
+                    if (this.result.data.message[item]) {
+                        this.errors.push(this.result.data.message[item][0]);
+                        this.hasError = false;
+                    }
+                }
+
+                this.hasError = true;
+                if (this.errors.length == 0) {
+                    this.hasError = false;
+                    this.errors.push('Project was updates');
+                }
+
+                this.messageIsActive = true;
             }
         }
     }
